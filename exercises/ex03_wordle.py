d39c74edd45ea6dd9051266c70ab46782046791e
Sydney Rogers <srogers2@unc.edu>
2023-02-26 14:33:17 -0500

    Exercise 4

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -3,29 +3,16 @@
 __author__ = "730466526"
 
 
-def contains_char(word: str, letter: str) -> bool:
+def contains_char(secret: str, character: str) -> bool:
     """Finding the letter in the word."""
-    assert len(letter) == 1
+    assert len(character) == 1
     counter: int = 0
-    perfect: bool = True
-    while counter < len(word):
-        if word[counter] == letter:
-            perfect = True
-        else:
-            track: int = 0
-            existence: bool = False
-            while ((existence is False) & (track < len(word))):
-                if word[counter] == letter:
-                    existence = True
-                else:
-                    track += 1
-                    counter += 1
-            track = 0
-        if existence is True:
-            perfect = True
-        else:
-            perfect = False
-    return perfect
+    while counter < len(secret):
+        if secret[counter] == character:
+            return True
+        counter += 1
+    return False
+
 
 def emojified(guess: str, secret: str) -> str:
     """Inserts Emoji Colored Boxes."""
@@ -34,62 +21,46 @@ def emojified(guess: str, secret: str) -> str:
     green_box: str = "\U0001F7E9"
     yellow_box: str = "\U0001F7E8"
     emoji: str = ""
-    counter: int = 0
-    if guess == secret:
-        while counter < len(secret):
-            if guess[counter] == secret[counter]:
+    idx: int = 0
+    while idx < len(secret):
+        if contains_char(secret, guess[idx]) is True:
+            if guess[idx] == secret[idx]:
                 emoji += green_box
-            counter = counter + 1
-        return emoji
-    while counter < len(secret):
-        if guess[counter] == secret[counter]:
-            emoji += green_box
-        else:
-            asses = contains_char(secret, guess[counter])
-            if asses is True:
-                emoji += yellow_box
             else:
-                emoji += white_box
-        counter += 1
+                emoji += yellow_box
+        else:
+            emoji += white_box
+        idx += 1
     return emoji
 
 
-def input_guess(number: int) -> str:
+def input_guess(length: int) -> str:
     """Asks user for a correct in length guess."""
-    word: str = input(f"Enter a {number} charcter word: ")
-    correct: str = ""
-    while len(word) != number:
-        guess: str = input(f"That wasn't {number} chars! Try again: ")
-    if len(word) == number:
-        correct = word
-    return correct
+    guess: str = input(f"Enter a {length} character word: ")
+    while len(guess) != length:
+        guess_2: str = input(f"That wasn't {length} chars! Try again: ")
+        guess = guess_2
+    return guess
 
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     secret: str = "codes"
-    counter: int = 1
-    green_box: str = "\U0001F7E9"
-    emoji: str = ""
-    while counter <= 6:
-        turn: str = (f"=== Turn {counter}/6 ===")
-        print(f"{turn}")
-        guess = input_guess(5)
-        if secret == guess:
-            i: int = 0
-            while i < len(secret):
-                if guess[i] == secret[i]:
-                    emoji += green_box
-                i = i + 1
-            print(f"{emoji}")
-            print(f"You won in {counter}/6 turns!")
-            break
+    turn: int = 1
+    exit_loop: bool = False
+    while turn <= 6 and exit_loop is False:
+        print(f"=== Turn {turn}/6 ===")
+        main_guess: str = input_guess(len(secret))
+        print(emojified(main_guess, secret))
+        if main_guess == secret:
+            exit_loop = True
         else:
-            result = emojified(guess, secret)
-            print(f"{result}")
-        counter = counter + 1
-
-    if counter >= 6:
+            turn += 1
+    if main_guess == secret:
+        print(f"You won in {turn}/6 turns!")
+    if turn > len(secret):
         print("X/6 - Sorry, try again tomorrow!")
+
+
 if __name__ == "__main__":
     main()
---
+++ exercises/ex04_utils.py
@@ -0,0 +1,35 @@
+"""Practice with Lists."""
+
+__author__ = "730466526"
+
+
+def all(a: list[int], b: int) -> bool:
+    """Indicate whether or not all the ints in the list are the same as the given int."""
+    counter: int = 0
+    if len(a) == counter:
+        return False
+    while counter < len(a):
+        if a[counter] != b:
+            return False
+        counter += 1
+    return True
+
+
+def max(input: list[int]) -> int:
+    """Returns the largest int in the list."""
+    if len(input) == 0:
+        raise ValueError("max() arg is an emoty List")
+    large = input[0]
+    for number in input:
+        if number > large:
+            large = number
+    return large
+
+
+def is_equal(c: list[int], d: list[int]) -> bool:
+    """Indicate whether or not the lists are equal."""
+    if c == d:
+        return True
+    else:
+        return False
+
